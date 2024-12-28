30753e0984205edbc3408ae46238a2ee4d9f3304
kirankalluru <129094970+kirankalluru@users.noreply.github.com>
2024-12-28 21:34:43 +0530

    Arraylist

--- Arrays/ArraylistExample.java
+++ Arrays/ArraylistExample.java
@@ -1,7 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
-
-
 public class ArraylistExample {
 
     public static void main(String[] args) {
--- pascals.java
+++ pascals.java
@@ -43,3 +43,16 @@ public class pascals {
         sc.close();
     }
 }
+
+// output
+
+// Enter the number of rows:
+// 5
+//      1 
+//     1 1 
+//    1 2 1 
+//   1 3 3 1
+//  1 4 6 4 1
+// [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+
+
